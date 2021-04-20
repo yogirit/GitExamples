0cc7bc682feafa22abfd5c2d20caac8be3d44e31
yogirit <yogeshbhargavidea@gmail.com>
2021-04-20 16:02:59 +0530

    done

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee{
+static String name;
+static int id;
+public static void main(String emp[]){
+System.out.println("Employee Name "+Employee.name);
+System.out.println("Employee Id "+Employee.id);
+
+
+}
+}
